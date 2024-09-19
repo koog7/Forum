a950746017bef86037e3f46ec637841542bd57a3
koog7 <k.ognev709@mail.com>
2024-09-19 12:53:31 +0600

    Написан метод get Для получения всех постов

--- backend/routes/ForumThemeRoutes.ts
+++ backend/routes/ForumThemeRoutes.ts
@@ -6,6 +6,20 @@ import {imagesUpload} from "../multer";
 const ForumThemeRouter = express.Router();
 ForumThemeRouter.use(express.json());
 
+ForumThemeRouter.get('/' , async (req ,res ,next) =>{
+    try{
+        const allPosts = await Forum.find().sort({date:-1});
+
+        if(!allPosts){
+            return res.status(400).send({error: 'Some problems with fetching all post'})
+        }
+
+        res.send(allPosts)
+    }catch (e) {
+        next(e)
+    }
+})
+
 ForumThemeRouter.post('/', imagesUpload.single('photo'), async (req,res,next) => {
     const getToken = req.get('Authorization');
 
